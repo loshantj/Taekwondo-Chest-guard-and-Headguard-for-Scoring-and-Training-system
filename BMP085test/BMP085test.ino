1f031c1007017778d3c23e5797656ce304de0886
loshantj <loshantj-pe18020@stu.kln.ac.lk>
2023-03-08 14:04:20 +0530

    arduino files are added

---
+++ BMP085test/BMP085test.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
+}
+
+void loop() {
+  float pressure = bmp.readPressure() / 100.0F; // convert to hPa
+  Serial.print("Pressure = ");
+  Serial.print(pressure);
+  Serial.println(" hPa");
+
+  delay(1000);
+}
